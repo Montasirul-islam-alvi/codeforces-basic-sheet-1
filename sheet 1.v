c80eef6dfb50fbd6c05123a18f10e0dc426011f2
Montasirul-islam-alvi <109718858+Montasirul-islam-alvi@users.noreply.github.com>
2022-09-28 23:31:59 +0600

    Create sheet 1.v

---
+++ sheet 1.v
@@ -0,0 +1,34 @@
+# codeforces-basic-sheet-1
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    char ch;
+    scanf("%d %c %d",&a,&ch,&b);
+    if(ch == '>'){
+        if(a>b){
+            printf("Right\n");
+        }
+        else{
+            printf("Wrong\n");
+        }
+    }
+
+    else if(ch=='<'){
+        if(a<b){
+            printf("Right\n");
+        }
+        else{
+            printf("Wrong\n");
+        }
+    }
+
+    else if(ch == '='){
+        if(a==b){
+            printf("Right\n");
+        }
+        else{
+            printf("Wrong\n");
+        }
+    }
+}
